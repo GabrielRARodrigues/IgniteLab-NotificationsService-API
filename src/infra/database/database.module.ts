960ba9dbb286b4aa3391472c0eed59d9df2534ef
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2022-12-15 22:06:01 -0300

    Criando as entidades do projeto, testando as entidades e funcionalidades, e preparando o terreno para a integração de banco de dados

---
+++ src/infra/database/database.module.ts
@@ -0,0 +1,15 @@
+import { PrismaNotificationsRepository } from './Prisma/repositories/prisma-notifications-repository';
+import { NotificationsRepository } from 'src/application/repositories/notification-repositories';
+import { Module } from '@nestjs/common';
+import { PrismaService } from './Prisma/prisma.service';
+
+@Module({
+  providers: [PrismaService,{
+    provide:NotificationsRepository,
+    useClass: PrismaNotificationsRepository
+  }],
+  exports: [
+    NotificationsRepository
+  ]
+})
+export class DatabaseModule {}
